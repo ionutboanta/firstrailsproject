72ed6bda20d07b5e8febaa51984c0cb57e2f8ad0
Alex Harastasan <alexandru.haratasan@crisser.ro>
2017-02-02 14:46:30 +0000

    Added edit article function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,10 @@ class ArticlesController < ApplicationController
         
     end
     
+    def edit
+       @article = Article.find(params[:id]) 
+    end
+    
     def create
         @article = Article.new(article_params)
         if @article.save
@@ -18,6 +22,16 @@ class ArticlesController < ApplicationController
     def show
        @article = Article.find(params[:id]) 
         
+    end
+    
+    def update
+       @article = Article.find(params[:id])
+       if @article.update(article_params)
+           flash[:notice] = "Article was updated"
+           redirect_to article_path(@article)
+       else
+           render 'edit'
+       end
     end
     
     private
