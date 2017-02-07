a1adfdfd6abb4c2417426d7df54c4d25528658f1
Alex Harastasan <alexandru.haratasan@crisser.ro>
2017-02-07 10:49:27 +0000

    Assoc betwen user and articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206134453) do
+ActiveRecord::Schema.define(version: 20170207102814) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
