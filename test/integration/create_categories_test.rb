ca453b4cf9daaa9a18baeb68f3cf7b9aadee84ba
Alex Harastasan <alexandru.haratasan@crisser.ro>
2017-02-16 11:57:15 +0000

    Add admin req to create categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -4,7 +4,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
    
    test "get new category form and create category" do
        get new_category_path
-       assert_template 'categories/new'
+       assert_template "categories/new"
        assert_difference 'Category.count', 1 do
           post_via_redirect categories_path, category: {name: "sports"} 
        end
@@ -14,7 +14,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
    
     test "invalid category submission results in failure" do
       get new_category_path
-       assert_template 'categories/new'
+       assert_template "categories/new"
        assert_no_difference 'Category.count' do
           post categories_path, category: {name: " "} 
        end
